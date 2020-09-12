aec18c6ad27a298e621fde0c74c01e27347d824c
Rob Little <rlittletht@users.noreply.github.com>
2020-09-12 16:01:27 -0700

    reformatting only

--- module/downloads.js
+++ module/downloads.js
@@ -7,55 +7,61 @@ const limiter = new rateLimiter(1,1000);
 const download = require('image-downloader');
 const downloadDir = process.cwd()+"\\downloads\\";
 
-function startDownloads(imageList, folder){
-  return new Promise((resolve, reject)=>{
-    var counter = 1;
-    var maxCounter = imageList.length;
-    fs.mkdir(downloadDir+folder+"\\", (err)=>{
-      if(err){
-        if (err.code != 'EEXIST') reject(err);
-      }
-    })
-    imageList.map((i, index)=>{
-      var fileName = i.split("/")[i.split("/").length-1];
-      var fullPath = downloadDir+folder+"\\"+fileName;
-      fs.stat(fullPath,(err, stat)=>{
-        if(err == null){
-          console.log(`${i} downloaded, already exists: ${counter}/${maxCounter}!`);
-          counter++;
-          if(counter == maxCounter){
-            resolve();
-          }
-        }else if(err.code == "ENOENT"){        
-          limiter.removeTokens(1,()=>{
-            download
-            .image({url:i, dest: downloadDir+folder+"\\"})
-            .then((filename)=>{
-              console.log(`> ${i} downloaded, finished: ${counter}/${maxCounter}!`);
-              counter++;
-              if(counter == maxCounter){
-                resolve();
-              }
-            })
-            .catch((error)=>{
-              console.log(`${i} downloaded, failed to download: ${counter}/${maxCounter} [${error}]!`);
-              counter++;
-              if(counter == maxCounter){
-                resolve();
-              }
-            });
-          })
-        }else{
-          console.log(`${i} downloaded, already exists: ${counter}/${maxCounter}!`);
-          counter++;
-          if(counter == maxCounter){
-                resolve();
-          }
-        }
-      })
-    }) 
-  })
+function startDownloads(imageList, folder)
+{
+    return new Promise((resolve, reject) =>
+                       {
+                           var counter = 1;
+                           var maxCounter = imageList.length;
+                           fs.mkdir(downloadDir + folder + "\\", (err) => {
+                               if (err) {
+                                   if (err.code != 'EEXIST')
+                                       reject(err);
+                               }
+                           })
+                           imageList.map((i, index) => {
+                               var fileName = i.split("/")[i.split("/").length - 1];
+                               var fullPath = downloadDir + folder + "\\" + fileName;
+                               fs.stat(fullPath, (err, stat) => {
+                                   if (err == null) {
+                                       console.log(`${i} downloaded, already exists: ${counter}/${maxCounter}!`);
+                                       counter++;
+                                       if (counter == maxCounter) {
+                                           resolve();
+                                       }
+                                   }
+                                   else if (err.code == "ENOENT") {
+                                       limiter.removeTokens(1, () => {
+                                           download
+                                           .image({ url: i, dest: downloadDir + folder + "\\" })
+                                           .then((filename) => {
+                                               console.log(`> ${i} downloaded, finished: ${counter}/${maxCounter}!`);
+                                               counter++;
+                                               if (counter == maxCounter) {
+                                                   resolve();
+                                               }
+                                           })
+                                           .catch((error)=>{
+                                               console.log(`${i} downloaded, failed to download: ${counter}/${maxCounter} [${error}]!`);
+                                               counter++;
+                                               if (counter == maxCounter) {
+                                                   resolve();
+                                               }
+                                           });
+                                       })
+                                   }
+                                   else {
+                                       console.log(`${i} downloaded, already exists: ${counter}/${maxCounter}!`);
+                                       counter++;
+                                       if (counter == maxCounter) {
+                                           resolve();
+                                       }
+                                   }
+                               })
+                           })
+                       })
 }
 
- 
+
 module.exports.startDownloads = startDownloads
+    
