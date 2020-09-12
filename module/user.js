d1b361dd3c61badf35febf195661c2c3ec2120ee
Rob Little <rlittletht@users.noreply.github.com>
2020-09-12 16:34:17 -0700

    Create user.js

---
+++ module/user.js
@@ -0,0 +1,44 @@
+const snekfetch = require("snekfetch")
+const Promise = require("promise")
+const rateLimiter = require('limiter').RateLimiter
+const limiter = new rateLimiter(1,2500);
+
+/*
+function getGuild(token, guildId){
+  return new Promise((resolve, reject)=>{
+    limiter.removeTokens(1,()=>{
+      snekfetch.get('https://discordapp.com/api/guilds/'+guildId)
+      .set("Authorization",token)
+      .then((res)=>{resolve(res.body)})
+      .catch((e)=>{reject(e)})
+    })
+  })
+}
+
+function getGuilds(token){
+  return new Promise((resolve, reject)=>{
+    limiter.removeTokens(1,()=>{
+      snekfetch.get('https://discordapp.com/api/users/@me/guilds')
+      .set("Authorization",token)
+      .then((res)=>{resolve(res.body)})
+      .catch((e)=>{reject(e)})
+    })
+  })
+}
+
+*/
+function getUserChannels(token){
+  return new Promise((resolve, reject)=>{
+    limiter.removeTokens(1,()=>{
+      snekfetch.get('https://discordapp.com/api/users/@me/channels')
+      .set("Authorization",token)
+      .then((res)=>{resolve(res.body)})
+      .catch((e)=>{reject(e)})
+    })
+  })
+}
+
+/*module.exports.getGuild = getGuild;
+module.exports.getGuilds = getGuilds;
+*/
+module.exports.getUserChannels = getUserChannels;
